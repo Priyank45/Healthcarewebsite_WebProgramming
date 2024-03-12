556fbf448837d56f2b09c303434b045ba3a749d0
Priyank45 <priyanksharma.ps07@gmail.com>
2024-03-13 05:12:17 +0530

    feat: emailjs

--- index.js
+++ index.js
@@ -8,3 +8,62 @@ navbar.classList.toggle('navhresp');
 navList.classList.toggle('navoresp');
 navRight.classList.toggle('navoresp');
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".navoresp");
+
+    hamburger.addEventListener("click", function() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+
+    document.querySelectorAll(".navList a").forEach(n => n.addEventListener("click", function() {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }));
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+const form = document.querySelector(".Form-container form");
+const nameInput = form.querySelector('input[type="text"]');
+const numberInput = form.querySelector('input[type="number"]');
+const emailInput = form.querySelector('input[type="email"]');
+const dateInput = form.querySelector('input[type="date"]');
+const messageTextarea = form.querySelector('textarea');
+
+// form.addEventListener("submit", function(e) {
+//     e.preventDefault(); 
+
+    
+//     if(nameInput.value.trim() === "") {
+//         alert("Please enter your name.");
+//         nameInput.focus();
+//         return false;
+//     }
+
+   
+    // alert("Appointment booked successfully!");
+    
+//     form.reset(); 
+// });
+
+
+const searchInput = document.getElementById("search");
+const searchButton = document.querySelector(".btnSearch");
+
+searchButton.addEventListener("click", function() {
+    const query = searchInput.value.trim();
+    alert(`Search for: ${query}`);
+   
+});
---
+++ sendmail.js
@@ -0,0 +1,24 @@
+function SendMail(){
+    var params = {
+        from_name: "Priyank Sharma",
+        to_name : document.getElementById("fullName").value,
+        phone_number : document.getElementById("number").value,
+        email_id : document.getElementById("email_id").value,
+        date_appointment : document.getElementById("date").value,
+        message : document.getElementById("message").value 
+    }
+    try{
+        emailjs.send("service_fbugajd", "template_ckvu8j2", params).then(function(res){
+            alert("You're appointment has been scheduled" + res.status);
+            console.log("sent");
+        })
+    }
+
+    catch{
+        console.log(error());
+    }
+        
+    
+    
+        
+}
